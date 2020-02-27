be71b0d2bdfc172cb8682904b65117813b89e768
polojerry <polojerry4@gmail.com>
2020-02-27 20:36:08 +0300

    Updated Navigation Implementation

--- app/src/main/java/com/rabitech/mara/MainActivity.kt
+++ app/src/main/java/com/rabitech/mara/MainActivity.kt
@@ -2,9 +2,11 @@ package com.rabitech.mara
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
@@ -17,17 +19,36 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var drawerlayout: DrawerLayout
+    lateinit var navController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        navController = findNavController(R.id.myNavHostFragment)
 
         setupDrawer()
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id == R.id.login){
+                binding
+                    .apply {
+                        toolbarMain.visibility = View.GONE
+                        navView.visibility = View.GONE
+                    }
+            }else{
+                binding
+                    .apply {
+                        toolbarMain.visibility = View.VISIBLE
+                        navView.visibility = View.VISIBLE
+                    }
+            }
+        }
+
+
     }
 
     private fun setupDrawer() {
         drawerlayout = binding.drawerLayout
-        val navController = findNavController(R.id.myNavHostFragment)
         val toolbar = findViewById<Toolbar>(R.id.toolbar_main)
         val appBarConfiguration = AppBarConfiguration(navController.graph, drawerlayout)
 
